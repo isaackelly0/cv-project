fc77cf679e201884d0d41e29afe915782f993717
Isaac Kelly <isaacmkelly0@gmail.com>
2020-12-31 15:56:45 -0600

    experimenting around with react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+import Resume from "./components/Resume";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { form: "" };
+  }
+  handleChange = (e) => {
+    this.setState({ form: e.target.value });
+  };
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    ReactDOM.render(
+      <Resume form={this.state.form} />,
+      document.getElementById("output")
+    );
+    this.setState({ form: "" });
+  };
+  render() {
+    const { form } = this.state;
+    return (
+      <div className="App">
+        <form onSubmit={this.onSubmitForm}>
+          <input
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.form}
+          ></input>
+          <button type="submit">ENTER</button>
+        </form>
+        <div id="output"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
