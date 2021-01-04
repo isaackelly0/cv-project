ab1d506d575403b82b70d6bb719dee9e7b2324de
Isaac Kelly <isaacmkelly0@gmail.com>
2021-01-04 16:01:48 -0600

    rendering other components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,44 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
-
-import Resume from "./components/Resume";
+//import ReactDOM from "react-dom";
+import Work from "./components/work";
+import Info from "./components/info";
+import Education from "./components/education";
+//import Resume from "./components/Resume";
 
 class App extends Component {
   constructor() {
     super();
     this.state = { form: "" };
   }
-  handleChange = (e) => {
-    this.setState({ form: e.target.value });
-  };
-  onSubmitForm = (e) => {
-    e.preventDefault();
-    ReactDOM.render(
-      <Resume form={this.state.form} />,
-      document.getElementById("output")
-    );
-    this.setState({ form: "" });
-  };
+  // handleChange = (e) => {
+  //   this.setState({ form: e.target.value });
+  // };
+  // onSubmitForm = (e) => {
+  //   e.preventDefault();
+  //   ReactDOM.render(
+  //     <Resume form={this.state.form} />,
+  //     document.getElementById("output")
+  //   );
+  //   this.setState({ form: "" });
+  // };
   render() {
-    const { form } = this.state;
+    //const { form } = this.state;
     return (
       <div className="App">
-        <form onSubmit={this.onSubmitForm}>
-          <input
-            type="text"
-            onChange={this.handleChange}
-            value={this.state.form}
-          ></input>
-          <button type="submit">ENTER</button>
-        </form>
-        <div id="output"></div>
+        <Work />
+        <Info />
+        <Education />
       </div>
+      // <form onSubmit={this.onSubmitForm}>
+      //   <input
+      //     type="text"
+      //     onChange={this.handleChange}
+      //     value={this.state.form}
+      //   ></input>
+      //   <button type="submit">ENTER</button>
+      // </form>
+      // <div id="output"></div>
+      //</div>
     );
   }
 }
