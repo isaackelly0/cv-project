c7e2bf32d6ed0ca31c3563c2fd096549b262829b
Isaac Kelly <isaacmkelly0@gmail.com>
2021-01-07 11:51:40 -0600

    added the rest of the form from the deleted files

--- src/App.js
+++ src/App.js
@@ -36,13 +36,40 @@ class App extends Component {
     //const { form } = this.state;
     return (
       <form onSubmit={this.submitForm} className="App">
+        <p>Name:</p>
+        <input type="text" onChange={this.handleChange} name="name"></input>
+        <p>Email:</p>
+        <input type="text" onChange={this.handleChange} name="email"></input>
+        <p>Phone Number:</p>
+
+        <input type="text" onChange={this.handleChange} name="number"></input>
+        <p>School Name:</p>
+
+        <input type="text" onChange={this.handleChange} name="school"></input>
+        <p>Field of Study:</p>
+
+        <input type="text" onChange={this.handleChange} name="study"></input>
+        <p>Date of Study:</p>
+
         <input
           type="text"
           onChange={this.handleChange}
-          name="firstName"
+          name="dateOfStudy"
         ></input>
-        <input type="text" onChange={this.handleChange} name="email"></input>
-        <input type="text" onChange={this.handleChange} name="number"></input>
+        <p>Company Name:</p>
+        <input type="text" onChange={this.handleChange} name="company"></input>
+        <p>Position:</p>
+        <input type="text" onChange={this.handleChange} name="position"></input>
+        <p>Tasks:</p>
+        <input type="text" onChange={this.handleChange} name="tasks"></input>
+        <p>Start Date:</p>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          name="startDate"
+        ></input>
+        <p>End Date:</p>
+        <input type="text" onChange={this.handleChange} name="endDate"></input>
         <button>ENTER</button>
       </form>
       // <Work />
