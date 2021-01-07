339fe1e583e23bf5b3cebff5258ab5af4571e21e
Isaac Kelly <isaacmkelly0@gmail.com>
2021-01-07 14:12:52 -0600

    deleted info and outdated references to other files from App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 //import ReactDOM from "react-dom";
-//import Work from "./components/work";
-//import Info from "./components/info";
-//import Education from "./components/education";
 //import Resume from "./components/Resume";
 
 class App extends Component {
@@ -11,7 +8,6 @@ class App extends Component {
     this.state = {};
     this.submitForm = (e) => {
       e.preventDefault();
-      //alert(this.state);
       for (const key in this.state) {
         console.log(key + ":" + this.state[key]);
       }
@@ -24,14 +20,6 @@ class App extends Component {
     const target = e.target;
     this.setState({ [target.name]: target.value });
   };
-  // onSubmitForm = (e) => {
-  //   e.preventDefault();
-  //   ReactDOM.render(
-  //     <Resume form={this.state.form} />,
-  //     document.getElementById("output")
-  //   );
-  //   this.setState({ form: "" });
-  // };
   render() {
     //const { form } = this.state;
     return (
@@ -72,20 +60,6 @@ class App extends Component {
         <input type="text" onChange={this.handleChange} name="endDate"></input>
         <button>ENTER</button>
       </form>
-      // <Work />
-      //
-      // <Education />
-      // <button>ENTER</button>
-      // <form onSubmit={this.onSubmitForm}>
-      //   <input
-      //     type="text"
-      //     onChange={this.handleChange}
-      //     value={this.state.form}
-      //   ></input>
-      //   <button type="submit">ENTER</button>
-      // </form>
-      // <div id="output"></div>
-      //</div>
     );
   }
 }
