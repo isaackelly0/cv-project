a0995dbba1da4372a8be9d21cb69de96e8327676
Isaac Kelly <isaacmkelly0@gmail.com>
2021-01-13 16:19:42 -0600

    nested elements in both js components, added a css file

--- src/App.js
+++ src/App.js
@@ -54,52 +54,81 @@ class App extends Component {
           className="App"
           style={this.state.form}
         >
-          <p>Name:</p>
-          <input type="text" onChange={this.handleChange} name="name"></input>
-          <p>Email:</p>
-          <input type="text" onChange={this.handleChange} name="email"></input>
-          <p>Phone Number:</p>
+          <div>
+            <h1>Contact Info</h1>
+            <p>Name:</p>
+            <input type="text" onChange={this.handleChange} name="name"></input>
+            <p>Email:</p>
+            <input
+              type="text"
+              onChange={this.handleChange}
+              name="email"
+            ></input>
+            <p>Phone Number:</p>
 
-          <input type="text" onChange={this.handleChange} name="number"></input>
-          <p>School Name:</p>
+            <input
+              type="text"
+              onChange={this.handleChange}
+              name="number"
+            ></input>
+          </div>
+          <div>
+            <h1>Education</h1>
+            <p>School Name:</p>
 
-          <input type="text" onChange={this.handleChange} name="school"></input>
-          <p>Field of Study:</p>
+            <input
+              type="text"
+              onChange={this.handleChange}
+              name="school"
+            ></input>
+            <p>Field of Study:</p>
 
-          <input type="text" onChange={this.handleChange} name="study"></input>
-          <p>Date of Study:</p>
+            <input
+              type="text"
+              onChange={this.handleChange}
+              name="study"
+            ></input>
+            <p>Date of Study:</p>
 
-          <input
-            type="text"
-            onChange={this.handleChange}
-            name="dateOfStudy"
-          ></input>
-          <p>Company Name:</p>
-          <input
-            type="text"
-            onChange={this.handleChange}
-            name="company"
-          ></input>
-          <p>Position:</p>
-          <input
-            type="text"
-            onChange={this.handleChange}
-            name="position"
-          ></input>
-          <p>Tasks:</p>
-          <input type="text" onChange={this.handleChange} name="tasks"></input>
-          <p>Start Date:</p>
-          <input
-            type="text"
-            onChange={this.handleChange}
-            name="startDate"
-          ></input>
-          <p>End Date:</p>
-          <input
-            type="text"
-            onChange={this.handleChange}
-            name="endDate"
-          ></input>
+            <input
+              type="text"
+              onChange={this.handleChange}
+              name="dateOfStudy"
+            ></input>
+          </div>
+          <div>
+            <h1>Work History</h1>
+            <p>Company Name:</p>
+            <input
+              type="text"
+              onChange={this.handleChange}
+              name="company"
+            ></input>
+            <p>Position:</p>
+            <input
+              type="text"
+              onChange={this.handleChange}
+              name="position"
+            ></input>
+            <p>Tasks:</p>
+            <input
+              type="text"
+              onChange={this.handleChange}
+              name="tasks"
+            ></input>
+            <p>Start Date:</p>
+            <input
+              type="text"
+              onChange={this.handleChange}
+              name="startDate"
+            ></input>
+            <p>End Date:</p>
+            <input
+              type="text"
+              onChange={this.handleChange}
+              name="endDate"
+            ></input>
+          </div>
           <button>ENTER</button>
         </form>
         <span id="resume" style={this.state.resume}>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,23 +3,34 @@ class Resume extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.reset = () => {
+      this.props.callback("false");
+    };
   }
   render() {
     return (
       <span>
-        <p>Name: {this.props.name}</p>
-        <p>Email: {this.props.email}</p>
-        <p>Phone Number: {this.props.number}</p>
-        <p>School: {this.props.school}</p>
-        <p>Field of Study{this.props.study}</p>
-        <p>Study Date{this.props.dateOfStudy}</p>
-        <p>Company Name: {this.props.company}</p>
-        <p>Position: {this.props.position}</p>
-        <p>Tasks performed: {this.props.tasks}</p>
-        <p>Start: {this.props.startDate}</p>
-        <p>End: {this.props.endDate}</p>
-        <button onClick={this.props.callback("false")}>EDIT</button> //error
-        //thrown, need to define this function better
+        <div>
+          <h1>Contact Info</h1>
+          <p>Name: {this.props.name}</p>
+          <p>Email: {this.props.email}</p>
+          <p>Phone Number: {this.props.number}</p>
+        </div>
+        <div>
+          <h1>Education</h1>
+          <p>School: {this.props.school}</p>
+          <p>Field of Study{this.props.study}</p>
+          <p>Study Date{this.props.dateOfStudy}</p>
+        </div>
+        <div>
+          <h1>Work History</h1>
+          <p>Company Name: {this.props.company}</p>
+          <p>Position: {this.props.position}</p>
+          <p>Tasks performed: {this.props.tasks}</p>
+          <p>Start: {this.props.startDate}</p>
+          <p>End: {this.props.endDate}</p>
+        </div>
+        <button onClick={this.reset}>EDIT</button>
       </span>
     );
   }
