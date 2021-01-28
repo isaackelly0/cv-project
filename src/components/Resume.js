b448269c363bd1289482d45242390a4b2d83077c
Isaac Kelly <isaacmkelly0@gmail.com>
2021-01-27 18:10:34 -0600

    added bootstrap classes to resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -11,27 +11,27 @@ class Resume extends Component {
   render() {
     return (
       <span>
-        <div>
+        <div className="row">
           <h1>Contact Info</h1>
-          <p>Name: {this.props.name}</p>
-          <p>Email: {this.props.email}</p>
-          <p>Phone Number: {this.props.number}</p>
+          <p className="col-lg-4 col-md-6">Name: {this.props.name}</p>
+          <p className="col-lg-4 col-md-6">Email: {this.props.email}</p>
+          <p className="col-lg-4 col-md-6">Phone Number: {this.props.number}</p>
         </div>
-        <div>
+        <div className="row">
           <h1>Education</h1>
-          <p>School: {this.props.school}</p>
-          <p>Field of Study{this.props.study}</p>
-          <p>Study Date{this.props.dateOfStudy}</p>
+          <p className="col-lg-4 col-md-6">School: {this.props.school}</p>
+          <p className="col-lg-4 col-md-6">Field of Study{this.props.study}</p>
+          <p className="col-lg-4 col-md-6">Study Date{this.props.dateOfStudy}</p>
         </div>
-        <div>
+        <div className="row">
           <h1>Work History</h1>
-          <p>Company Name: {this.props.company}</p>
-          <p>Position: {this.props.position}</p>
-          <p>Tasks performed: {this.props.tasks}</p>
-          <p>Start: {this.props.startDate}</p>
-          <p>End: {this.props.endDate}</p>
+          <p className="col-lg-4 col-md-6">Company Name: {this.props.company}</p>
+          <p className="col-lg-4 col-md-6">Position: {this.props.position}</p>
+          <p className="col-lg-4 col-md-6">Tasks performed: {this.props.tasks}</p>
+          <p className="col-lg-4 col-md-6">Start: {this.props.startDate}</p>
+          <p className="col-lg-4 col-md-6">End: {this.props.endDate}</p>
         </div>
-        <button onClick={this.reset}>EDIT</button>
+        <button className="btn btn-success" onClick={this.reset}>EDIT</button>
       </span>
     );
   }
